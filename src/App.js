3417c3ccc2729bf766823d6d6471adc4c4a226b2
timofey.podolenchuk <timofey.podolenchuk@x5.ru>
2021-04-12 20:27:39 +0300

    fix dates order

--- src/App.js
+++ src/App.js
@@ -1,23 +1,193 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {MenuItem, Select, Tooltip} from "@material-ui/core";
+import {MuiPickersUtilsProvider, KeyboardDatePicker} from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
+import {DatePicker} from "@material-ui/pickers";
+import {useEffect, useState} from "react";
+import {BarChart, Bar, CartesianGrid, Legend, ResponsiveContainer, XAxis, YAxis} from "recharts";
 
 function App() {
+
+  const codes = {
+    'MOW': "Moscow",
+    'LED': "Saint Petersburg",
+    'KZN': "Kazan",
+    'CEK': "Chelyabinsk",
+    'SVX': "Ekaterinburg",
+    'AER': "Sochi",
+    'KRR': "Krasnodar",
+    'KGD': "Kaliningrad",
+    'SGC': "Surgut",
+    'OVB': "Novosibirsk",
+    'VVO': "Vladivostok",
+    'YKS': "Yakutsk"
+  }
+
+  const codeKeys = Object.keys(codes)
+
+  const [origin, setOrigin] = useState(codeKeys[0])
+  const [destination, setDestination] = useState(codeKeys[1])
+  const [departureDate, setDepartureDate] = useState(new Date())
+
+  const onSetDestination = (event) => {
+    setDestination(event.target.value)
+  }
+
+  const onSetOrigin = (event) => {
+    setOrigin(event.target.value)
+  }
+
+  const onSetDepartureDate = (date) => {
+    setDepartureDate(date)
+  }
+
+  const [data, setData] = useState([])
+
+  const data1 = [
+    {
+      name: 'Page A',
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: 'Page B',
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: 'Page C',
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: 'Page D',
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: 'Page E',
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: 'Page F',
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: 'Page G',
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
+
+  useEffect(()=>{
+    getGraphData()
+  }, [departureDate, origin, destination])
+
+  const getGraphData = () => {
+    const today = new Date()
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("POST", "http://127.0.0.1:5000/predict_prices", true);
+    xmlHttp.setRequestHeader("Access-Control-Allow-Origin", "*")
+    xmlHttp.setRequestHeader("Content-type", "application/json")
+    xmlHttp.send(JSON.stringify({
+      "origin" : origin,
+      "destination": destination,
+      "date": `${today.getDate()}.${today.getMonth()}.${today.getFullYear().toString().slice(2,)}`,
+      "flight_date": `${departureDate.getDate()}.${departureDate.getMonth()}.${departureDate.getFullYear().toString().slice(2,)}`,
+    }));
+
+    xmlHttp.onerror = () =>{
+    }
+
+    xmlHttp.onload = () => {
+      let responseData = JSON.parse(xmlHttp.response)
+      let newData = []
+      for (let i = 0; i < responseData.days.length; i++) {
+        newData.unshift({
+          name: responseData.days[i],
+          prediction: responseData.prices[i],
+          realPrices: responseData.real_prices[i],
+        },)
+      }
+      setData(newData)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={"main-wrapper"}>
+        <div className={"selects"}>
+          <Select
+            className={"select"}
+            variant="outlined"
+            value={origin}
+            onChange={onSetOrigin}
+          >
+            {codeKeys.map(key => {
+              return <MenuItem value={key}>{codes[key]}</MenuItem>
+            })}
+          </Select>
+
+          <span className={"pointer"}>
+          →
+        </span>
+          <Select
+            className={"select"}
+            variant="outlined"
+            value={destination}
+            onChange={onSetDestination}
+          >
+            {codeKeys.map(key => {
+              return <MenuItem value={key}>{codes[key]}</MenuItem>
+            })}
+          </Select>
+        </div>
+
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          <DatePicker
+            className={"date-picker"}
+            autoOk
+            orientation="landscape"
+            variant="static"
+            openTo="date"
+            value={departureDate}
+            onChange={onSetDepartureDate}
+          />
+        </MuiPickersUtilsProvider>
+
+        <div className={"chart"}>
+          <ResponsiveContainer width="100%" height="100%">
+            <BarChart
+              width={500}
+              height={300}
+              data={data}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3"/>
+              <XAxis dataKey="name"/>
+              <YAxis/>
+              <Tooltip/>
+              <Legend/>
+              <Bar dataKey="prediction" fill="#8896E3"/>
+              <Bar dataKey="realPrices" fill="#3f51b5"/>
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
+      </div>
     </div>
   );
 }
